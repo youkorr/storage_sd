89a8332d7111d5f6769f7921209ec6cca54be63d
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-28 06:01:22 +0200

    Update __init__.py

--- esphome/components/storage/__init__.py
+++ esphome/components/storage/__init__.py
@@ -1,38 +1,25 @@
 import esphome.codegen as cg
+from esphome.components import storage
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome.cpp_generator import MockObjClass
 
-# Namespace et classes
-storage_ns = cg.esphome_ns.namespace("storage")
-Storage = storage_ns.class_("Storage", cg.Component)
-StorageClient = storage_ns.class_("StorageClient", cg.Component)
 
-# Configuration
-CONF_PREFIX = "path_prefix"
 
-# Schéma de configuration pour le composant storage
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(Storage),
-    cv.Required(CONF_PREFIX): cv.string,
-}).extend(cv.COMPONENT_SCHEMA)
+DEPENDENCIES = ["sd_mmc", "storage"]
+sd_mmc_storage = sd_mmc_ns.class_("SD_MMC_Storage", cg.Component)
 
-def storage_schema(class_: MockObjClass = Storage) -> cv.Schema:
-    """Schéma pour les clients de stockage"""
-    return cv.Schema({
-        cv.GenerateID(): cv.declare_id(class_),
-        cv.Required(CONF_PREFIX): cv.string,
-    }).extend(cv.COMPONENT_SCHEMA)
+
+CONFIG_SCHEMA = storage.storage_schema(sd_mmc_storage).extend(
+    {
+        cv.GenerateID(): cv.declare_id(sd_mmc_storage),
+        cv.Required(CONF_SD_MMC_ID): cv.use_id(SdMmc),
+    }
+)
+
 
 async def to_code(config):
-    """Fonction principale de génération du code"""
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    
-    # Configuration du préfixe de chemin
-    cg.add(var.set_path_prefix(config[CONF_PREFIX]))
-
-async def storage_to_code(config):
-    """Fonction pour configurer un client de stockage"""
-    parent = await cg.get_variable(config[CONF_ID])
-    return parent
+    sd_mmc_component = await cg.get_variable(config[CONF_SD_MMC_ID])
+    cg.add(var.set_sd_mmc(sd_mmc_component))
+    await storage.storage_to_code(config)
