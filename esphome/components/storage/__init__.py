691bb6a68f608c6e987f829179776ef214e8c598
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-28 03:48:03 +0200

    Update __init__.py

--- esphome/components/storage/__init__.py
+++ esphome/components/storage/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_PLATFORM
 
 DEPENDENCIES = ["esp32"]
 
@@ -11,43 +11,34 @@ SdMmcStorage = storage_ns.class_("SdMmcStorage", Storage)
 
 MULTI_CONF = True
 
-# Define the platform schemas
-PLATFORM_SCHEMAS = {
-    "sd_mmc": cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(SdMmcStorage),
-            cv.Required("path_prefix"): cv.string,
-            cv.Required("sd_mmc_id"): cv.use_id(cg.Component),
-        }
-    ).extend(cv.COMPONENT_SCHEMA),
-}
-
-def validate_platform(config):
-    if "platform" not in config:
-        raise cv.Invalid("Platform must be specified")
-    
-    platform = config["platform"]
-    if platform not in PLATFORM_SCHEMAS:
-        raise cv.Invalid(f"Unknown platform '{platform}'")
-    
-    return PLATFORM_SCHEMAS[platform](config)
+# Simple platform validation
+def validate_platform(value):
+    if value == "sd_mmc":
+        return value
+    raise cv.Invalid(f"Platform '{value}' is not supported")
 
 CONFIG_SCHEMA = cv.All(
     cv.ensure_list,
-    [validate_platform]
+    [
+        cv.Schema(
+            {
+                cv.Required(CONF_PLATFORM): validate_platform,
+                cv.GenerateID(): cv.declare_id(SdMmcStorage),
+                cv.Required("path_prefix"): cv.string,
+                cv.Required("sd_mmc_id"): cv.use_id(cg.Component),
+            }
+        ).extend(cv.COMPONENT_SCHEMA)
+    ],
 )
 
 async def to_code(config):
     for conf in config:
-        platform = conf["platform"]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        await cg.register_component(var, conf)
         
-        if platform == "sd_mmc":
-            var = cg.new_Pvariable(conf[CONF_ID])
-            await cg.register_component(var, conf)
-            
-            # Set path prefix
-            cg.add(var.set_path_prefix(conf["path_prefix"]))
-            
-            # Get SD MMC component reference
-            sd_mmc_component = await cg.get_variable(conf["sd_mmc_id"])
-            cg.add(var.set_sd_mmc_component(sd_mmc_component))
+        # Set path prefix
+        cg.add(var.set_path_prefix(conf["path_prefix"]))
+        
+        # Get SD MMC component reference
+        sd_mmc_component = await cg.get_variable(conf["sd_mmc_id"])
+        cg.add(var.set_sd_mmc_component(sd_mmc_component))
