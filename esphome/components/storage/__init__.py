7da18287588fe5e0705e66d0408688be150e7fe2
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-28 05:31:47 +0200

    Update __init__.py

--- esphome/components/storage/__init__.py
+++ esphome/components/storage/__init__.py
@@ -3,34 +3,36 @@ import esphome.config_validation as cv
 from esphome.const import CONF_ID
 from esphome.cpp_generator import MockObjClass
 
+# Namespace et classes
 storage_ns = cg.esphome_ns.namespace("storage")
-Storage = storage_ns.class_("Storage", cg.EntityBase)
-StorageClient = storage_ns.class_("StorageClient", cg.EntityBase)
-StorageClientStatic = storage_ns.namespace("StorageClient")
+Storage = storage_ns.class_("Storage", cg.Component)
+StorageClient = storage_ns.class_("StorageClient", cg.Component)
 
-IS_PLATFORM_COMPONENT = True
-
+# Configuration
 CONF_PREFIX = "path_prefix"
 
+# Schéma de configuration pour le composant storage
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(Storage),
+    cv.Required(CONF_PREFIX): cv.string,
+}).extend(cv.COMPONENT_SCHEMA)
 
-STORAGE_SCHEMA = cv.Schema(
-    {cv.GenerateID(): cv.declare_id(Storage), cv.Required(CONF_PREFIX): cv.string}
-)
+def storage_schema(class_: MockObjClass = Storage) -> cv.Schema:
+    """Schéma pour les clients de stockage"""
+    return cv.Schema({
+        cv.GenerateID(): cv.declare_id(class_),
+        cv.Required(CONF_PREFIX): cv.string,
+    }).extend(cv.COMPONENT_SCHEMA)
 
-
-def storage_schema(
-    class_: MockObjClass = cv.UNDEFINED,
-) -> cv.Schema:
-    schema = {}
-
-    if class_ is not cv.UNDEFINED:
-        # Not cv.optional
-        schema[cv.GenerateID()] = cv.declare_id(class_)
-
-    return STORAGE_SCHEMA.extend(schema)
-
+async def to_code(config):
+    """Fonction principale de génération du code"""
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    
+    # Configuration du préfixe de chemin
+    cg.add(var.set_path_prefix(config[CONF_PREFIX]))
 
 async def storage_to_code(config):
-    storage = await cg.get_variable(config[CONF_ID])
-    prefix = config[CONF_PREFIX]
-    cg.add(StorageClientStatic.add_storage(storage, prefix))
+    """Fonction pour configurer un client de stockage"""
+    parent = await cg.get_variable(config[CONF_ID])
+    return parent
